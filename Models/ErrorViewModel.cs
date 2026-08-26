b87d3f0de812a0588fb996a26672b2c185bee0f3
SylviaMari <152477676+SylviaBoss@users.noreply.github.com>
2026-08-26 14:08:41 +0200

    Legger til MVC-app og Dockerfile (#2)
    Co-authored-by: Ludolf0508 <sondrech@uia.no>

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace H_st_2026_Gruppe_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
