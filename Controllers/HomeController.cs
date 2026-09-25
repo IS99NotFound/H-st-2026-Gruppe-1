33824a31471d6b6f51e69c70ee9f0bcd55ab6ade
Steinar Alan Solberg Juhl <sajuhl@uia.no>
2026-09-25 19:51:23 +0200

    Revert "Merge pull request #4 from IS99NotFound/is99notfound-sync-firstwebapp-docker"
    This reverts commit ad0f45be87812792ccf1b1540c2ef2b867bc1c9b, reversing
    changes made to db7e0f565068688e4842d515e08be82278c7cb43.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,24 @@
-using FirstWebAppInDocker.Models;
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using H_st_2026_Gruppe_1.Models;
 
-namespace FirstWebAppInDocker.Controllers
+namespace H_st_2026_Gruppe_1.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace FirstWebAppInDocker.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace H_st_2026_Gruppe_1.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
